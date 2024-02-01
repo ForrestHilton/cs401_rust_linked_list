54adeeb41fa7d0bb61c20c8188450d8bf7d9e8b7
Forrest Hilton <forrestmhilton@gmail.com>
2024-02-01 10:40:36 -0600

    my interpretation of Gilrays plan

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use std::rc::Rc;
+use std::{io::Empty, rc::Rc};
 
 // enum is as large as its largest member
 // &List is likely to put the list on the stack
 // Box means that there is only one pointer
 #[derive(Debug)]
+#[derive(Clone)]
 enum List {
     Empty,
     Node(i64, Rc<List>),
@@ -16,20 +17,20 @@ impl List {
 
     fn len(&self) -> i32 {
         match self {
-            List::Node(_, next) => next.len()+1,
+            List::Node(_, next) => next.len() + 1,
             List::Empty => 0,
         }
     }
 
-    fn pushfront(self, _i: i64) -> Self {
-        List::Node(_i, Rc::new(self))
+    fn pushfront(&self, n1: i64) -> Self {
+        List::Node(n1, Rc::new(self.clone()))
     }
 }
 
 fn main() {
-    let lst = List::new();
-    let lst = lst.pushfront(4);
-    let lst = lst.pushfront(5);
+    let mut lst = List::new();
+    lst = lst.pushfront(4);
+    lst = lst.pushfront(5);
     println!("{:?}", lst.len());
     println!("{:?}", lst.len());
 }
