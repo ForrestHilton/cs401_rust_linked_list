a1e7926c586fdba77b8c38744efb03b80610d83c
Forrest Hilton <forrestmhilton@gmail.com>
2024-02-08 10:57:45 -0600

    dropfront

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 use std::rc::Rc;
 
+// an enum gives almost no encapsulation while dyn Trait gives a lot.
 #[derive(Debug, Clone)]
-struct List<T:Clone> {
+struct List<T: Clone> {
     link: Option<Rc<Cons<T>>>,
 }
 
 #[derive(Debug, Clone)]
-struct Cons<T:Clone> {
+struct Cons<T: Clone> {
     val: T,
     tail: List<T>,
 }
 
-impl<T:Clone> List<T> {
+impl<T: Clone> List<T> {
     fn new() -> Self {
         List { link: None }
     }
@@ -24,7 +25,23 @@ impl<T:Clone> List<T> {
     }
 
     fn pushfront(&self, n1: T) -> Self {
-        List {link : Some(Rc::new(Cons {val : n1, tail : self.clone()} ) ) }
+        List {
+            link: Some(Rc::new(Cons {
+                val: n1,
+                tail: self.clone(),
+            })),
+        }
+    }
+
+    fn dropfront(&self, n: usize) -> Self {
+        if n == 0 {
+            self.clone()
+        } else {
+            match self.link {
+                None => self.clone(),
+                Some(ref rc) => rc.tail.dropfront(n - 1),
+            }
+        }
     }
 }
 
